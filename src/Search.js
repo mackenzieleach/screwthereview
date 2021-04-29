f911b3daddc70dfc372ab6e13512cb29042a70e9
Mackenzie Leach <kenzieleach206@gmail.com>
2021-04-29 13:32:09 -0700

    Update Search.js with Rachel's Code

--- src/Search.js
+++ src/Search.js
@@ -1,14 +1,73 @@
 import React, { Component } from 'react';
+
  
+
 class Search extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Search</h1>
-                <p>Search page body content</p>
-            </div>
-        );
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {value: ''};
+
+    
+
+        this.handleChange = this.handleChange.bind(this);
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+
     }
+
+
+
+    handleChange(event) {
+
+        this.setState({value: event.target.value});
+
+    }
+
+    
+
+    handleSubmit(event) {
+
+    alert('A name was submitted: ' + this.state.value);
+
+    event.preventDefault();
+
+    }
+
+
+
+    render() {
+
+        return (
+
+            <div>
+
+                <h1>Search</h1>
+
+                <p>Search page body content</p>
+
+                <form onSubmit={this.handleSubmit}>
+
+                    <label>
+
+                    <input type="text" placeholder="Search keyword" value={this.state.value} onChange={this.handleChange} />
+
+                    </label>
+
+                    <input type="submit" value="Submit" />
+
+                </form>
+
+            </div>
+
+        );
+
+    }
+
 }
+
  
+
 export default Search;
