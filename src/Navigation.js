892d08150f8b334c9d8be130bcfcf6ee6e54790c
mmlea <leachm26@uw.edu>
2021-04-29 23:31:05 -0700

    Completed Dynamic Nav Bar & Footer

--- src/Navigation.js
+++ src/Navigation.js
@@ -4,30 +4,30 @@ import logo from './ScrewtheReviewLogo.png'
 function Navigation() {
     return (
         <nav className="navbar navbar-expand-sm navbar-light">
-            <a className="navbar-brand mr-3" href="/">
-                <img src={logo} alt="logo" style = {{ width:'150px'}}/>
+            <a className="navbar-brand mr-l" href="/">
+                <img src={logo} alt="logo" style={{ width: '150px' }} />
             </a>
-        
-            { /* Collapsable Button on Right */ }
+
+            { /* Collapsable Button on Right */}
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                        <a className="nav-link ml-2" href="/">Home <span class="sr-only">(current)</span></a>
+                <ul className="navbar-nav mr-auto" >
+                    <li className="nav-item ml-4">
+                        <a className="nav-link" href="/">Home</a>
                     </li>
-                    <li className="nav-item ml-2">
+                    <li className="nav-item ml-4" >
                         <a className="nav-link" href="/search">Search</a>
                     </li>
-                    <li className="nav-item ml-2">
+                    <li className="nav-item ml-4">
                         <a className="nav-link" href="/roulette">Roulette</a>
                     </li>
-                    <li className="nav-item ml-2">
+                    <li className="nav-item ml-4">
                         <a className="nav-link" href="/about">About</a>
                     </li>
-                    <li className="nav-item ml-2">
+                    <li className="nav-item ml-4">
                         <a className="nav-link" href="/account">Account</a>
                     </li>
                 </ul>
