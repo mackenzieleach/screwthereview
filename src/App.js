55b1d390291767591684c4446c64fd487890aeb6
Mackenzie Leach <kenzieleach206@gmail.com>
2021-05-03 14:41:54 -0700

    Updated Bootstrap Path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
+import './bootstrap/dist/css/bootstrap.min.css'
 import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
