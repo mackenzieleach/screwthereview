b01ec168873b5cb70755a862eb63b9fedae5a8e7
lulup2 <lulup2@uw.edu>
2021-05-04 13:52:55 -0700

    css and result page updates

--- src/Result.js
+++ src/Result.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import PandaExpress from './PandaExpress.png';
 import Map from './Map.png'
 import './App.css';
@@ -6,15 +7,15 @@ import './App.css';
 class Result extends Component {
     render() {
         return (
-            <div className="container">
-                <div className="row">
+            <Container>
+                <Row>
                     <div className="col font-large">Panda Express</div>
-                </div>
-                <div className="row">
-                    <div className="col-5">
+                </Row>
+                <Row>
+                    <Col sm={5}>
                         <img alt="panda express" src={PandaExpress} style={{ width: '-webkit-fill-available' }}/>
-                    </div>
-                    <div className="col-6">
+                    </Col>
+                    <Col sm={6}>
                         <div className="font-medium">About</div>
                         <div>Bringing people together to share joy has been our family’s inspiration
                         since the opening of the first Panda Express in 1983 in Glendale,
@@ -22,11 +23,11 @@ class Result extends Component {
                         and around the world, the bold flavors of Chinese cuisine inspire our
                         cooking, while our extended family of guests and associates remain
                         at the heart of what we do. Welcome to our family’s Chinese kitchen.</div>
-                        <div className="row">
-                            <div className="col-7">
+                        <Row>
+                            <Col sm={7}>
                                 <img alt="map" src={Map} className="img-responsive fit-image col-12" />
-                            </div>
-                            <div className="col-5">
+                            </Col>
+                            <Col sm={5}>
                                 <div className="font-medium">Hours</div>
                                 <div>Monday      10:30AM-9:30PM</div>
                                 <div>Tuesday     10:30AM-9:30PM</div>
@@ -35,22 +36,21 @@ class Result extends Component {
                                 <div>Friday      10:30AM-9:30PM</div>
                                 <div>Saturday    10:30AM-9:30PM</div>
                                 <div>Sunday      10:30AM-9:30PM</div>
-                            </div>
-                        </div>
-                    <div className="row">
-                            <div className="col-6">
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
                                 <div className="font-medium">Location</div>
                                 <div>12513 Lake City Way</div>
                                 <div>Seattle, WA 98123</div>
-                            </div>
-                            <div className="col-6">
-                                <span className="font-medium button">Get Directions
-                                </span>
-                            </div>
-                    </div>
-                </div>
-                </div>
-                </div>
+                            </Col>
+                            <Col>
+                                <button className="font-medium">Get Directions</button>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
