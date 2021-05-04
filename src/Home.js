2ddd5a9cabfc556620fe23f95eb102c8ad2e3c7d
mmlea <leachm26@uw.edu>
2021-05-04 00:07:08 -0700

    Bootstrapping for Landing Page

--- src/Home.js
+++ src/Home.js
@@ -9,59 +9,55 @@ import logo from './ScrewtheReviewLogo.png'
 class Home extends Component {
     render() {
         return (
-            <Container fluid style={{ paddingLeft: 30, paddingRight: 30, paddingTop: 30, paddingBottom: 30}}>
+            <Container fluid id = "home_container" style={{ paddingLeft: 40, paddingRight: 40, paddingTop: 30}} >
                 <Row>
                     {/* Column 1: Logo, Auth0 Widgets and Description*/}
-                    <Col align='center' style= {{paddingBottom: 20}} lg={6} md={12} sm={12} xs={12}>
-                        <img  src={logo} alt="Logo" style={{ width: '600px'}} />
-                        <h5 style={{textAlign: "center", paddingBottom: 20}}> Welcome to Screw the Review!</h5>
-                        <p> Screw the Review provides new experiences by randomly generating activities. The Roulette
+                    <Col id="landing_col_1" align='center' style={{paddingBottom: 20}} lg={6} md={12} sm={12} xs={12}>
+                        <img  src={logo} alt="Logo" id ="logo"/>
+                        <h5 id="welcome" style={{textAlign: "center", paddingBottom: 20}} > Welcome to Screw the Review!</h5>
+                        <p id="info"> Screw the Review provides new experiences by randomly generating activities. The Roulette
                             feature offers a totally randomized experience and Search generates an experience from filters
                             like price, location and keywords.
                         </p>
-                        <p>Click the tiles to learn more about Screw the Review and experience something new!</p>
+                        <p id="tiles">Click the tiles to learn more about Screw the Review and experience something new!</p>
 
                         <p>Place Holder: Auth0 Login and Signup Buttons</p>
                     </Col>
 
                     {/* Column 2: Search and About Us Cards*/}
-                    <Col lg={3} md={6} sm={6} xs ={6}>
+                    <Col lg={3} md={6} sm={12} xs ={12} style={{paddingBottom: 40}}>
                         {/* Search Card*/}
-                        <div class="card" style={{marginBottom: '20px'}}>
-                            <img class="card-img-top" src={logo} alt="Logo" style={{paddingTop: 10}}/>
-                            <div class="card-body">
-                                <p> Short Description for Search here </p>
-                                <a href="#" class="btn btn-primary">Search</a>
+                        <div className="card">
+                            <img className="card-img-top" src={logo} alt="Logo"/>
+                            <div className="card-body">
+                                <a href="#" class="btn btn-link btn-block">Search</a>
                             </div>
                         </div>
 
                         {/* About Us Card*/}
-                        <div class="card">
-                            <img class="card-img-top" src={logo} alt="Logo" style={{paddingTop: 10}} />
-                            <div class="card-body">
-                                <p> Short Description for About Us here </p>
-                                <a href="#" class="btn btn-primary">About Us</a>
+                        <div className="card">
+                            <img className="card-img-top" src={logo} alt="Logo"/>
+                            <div className="card-body">
+                                <a href="#" class="btn btn-link btn-block">About Us</a>
                             </div>
                         </div>
                     </Col>
 
                     {/* Column 3: Roulette and Contact Us Cards*/}
-                    <Col lg={3} md={6} sm={6} xs = {6}>
+                    <Col lg={3} md={6} sm={12} xs = {12} style={{paddingBottom: 40}}>
                         {/* Roulette Card*/}
-                        <div class="card" style={{marginBottom: '20px'}}>
-                            <img class="card-img-top" src={logo} alt="Logo" style={{paddingTop: 10}}/>
-                            <div class="card-body">
-                                <p> Short Description for Roulette here </p>
-                                <a href="#" class="btn btn-primary">Roulette</a>
+                        <div className="card">
+                            <img className="card-img-top" src={logo} alt="Logo"/>
+                            <div className="card-body">
+                                <a href="#" class="btn btn-link btn-block">Roulette</a>
                             </div>
                         </div>
 
                         {/* Contact Us Card*/}
-                        <div class="card">
-                            <img class="card-img-top" src={logo} alt="Logo" style={{paddingTop: 10}}/>
+                        <div className="card">
+                            <img class="card-img-top" src={logo} alt="Logo"/>
                             <div class="card-body">
-                                <p> Short Description for Contact here </p>
-                                <a href="#" class="btn btn-primary">Contact</a>
+                                <a href="#" class="btn btn-link btn-block">Contact</a>
                             </div>
                         </div>
                     </Col>
