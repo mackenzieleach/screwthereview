2d0c5228dd6148f8145014de02d7e6026a0f083b
Luke LeVasseur <lukelev@uw.edu>
2021-05-04 22:01:15 -0700

    Mocked up Yelp Scraping

--- src/Scrape.js
+++ src/Scrape.js
@@ -1,46 +1,64 @@
+require('dotenv').config()
 const axios = require('axios');
-const searchURL = "https://www.yelp.com/search";
+const baseURL = "https://api.yelp.com/v3/businesses/search";
 const descURL = "https://www.yelp.com/biz/";
 const loc = "Seattle"
 const category="restaurants"
 
-search(searchURL, loc, category).then( (res) => {
-    // ? find_desc=Restaurants & find_loc=Sacramento
-    const html = res.data;
-    console.log($('[class^="business"]').get());
-});
+// accept a category and location OR any of a location, category, price 
+// and will forward a description, addy, url, phone #, pricing, pictures, tags (type of cuisine, etc.), hours - In a Business Object
+getBiz([category], loc);
 
-async function search(searchURL, loc, category){
-    console.log("Making initial search")
+// Category and Location
+function getBiz(categories, loc){
+    var URL = baseURL + "?location=" + loc
+    if (categories.length >= 1){
+        URL = URL + "&categories=" + categories[0]
+        for (var i = 1; i < categories.length; i++){
+            URL = URL + "," + categories[i]
+        }
+    }
+    getExperience(URL);
+}
+
+function getExperience(searchURL){
+    console.log("Getting biz from Yelp Fusion")
     // make http call to url
-    let response = await axios(searchURL + "?find_desc=" + category + "&find_loc=" + loc).catch((err) => console.log(err));
+    axios.get(searchURL, {
+        headers: {
+            'Authorization' : 'Bearer ' + process.env.YELP_API_KEY
+        }
+    }).then(
+        (response) => {
+            var randomNum = Math.floor(Math.random() * response.data.businesses.length)
+            var randomBiz = response.data.businesses[randomNum]
+            // console.log(randomBiz)
+            // var bizDescription = scrapeDescription(randomBiz.id)
+            // randomBiz.push({
+            //     description: bizDescription
+            // });
+            console.log(randomBiz);
+            return randomBiz;
+        },
+        (error) => {
+            console.log(error);
+        }
+    );
+}
+
+// Scrapes description and hours from Yelp business page
+async function scrapeDescription(id){
+    let response = await axios(descURL + id).catch((err) => console.log(err));
 
     if(response.status !== 200){
         console.log("Error occurred while fetching data");
         return;
-    }
-    return response;
+    };
+    //TODO parse description and hours here
+    response = response.data
+    // console.log(response)
+    //hours table - Class contains OpenhoursOpenhoursrow
+    //description - Class starts w 
+    // $('[class^="business"]').get()
 }
 
-
-// fetchData(url).then( (res) => {
-//     const html = res.data;
-//     const $ = cheerio.load(html);
-//     const statsTable = $('.table.table-bordered.table-hover.downloads > tbody > tr');
-//     statsTable.each(function() {
-//         let title = $(this).find('td').text();
-//         console.log(title);
-//     });
-// })
-
-// async function fetchData(url){
-//     console.log("Scraping description")
-//     // make http call to url
-//     let response = await axios(url).catch((err) => console.log(err));
-
-//     if(response.status !== 200){
-//         console.log("Error occurred while fetching data");
-//         return;
-//     }
-//     return response;
-// }
