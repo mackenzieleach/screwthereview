f852620a5ba50b42692c4c4fe3bebcb3b094ad6f
mmlea <leachm26@uw.edu>
2021-05-09 16:15:58 -0700

    Fixed Bootstrapping in Search

--- src/Search.js
+++ src/Search.js
@@ -37,96 +37,95 @@ class Search extends Component {
     render() {
         return (
             <Container fluid id="search-container">
-                    {!this.state.seeResult ? (
-                        <Row>
-                            <Col lg={3} md={6} sm={12} xs={12}>
-                                <div>
-                                    <img className="card-img-top" src={museum} alt="museum" id="museum-pic" />
+                {!this.state.seeResult ? (
+                    <Row>
+                        <Col lg={3} md={6} sm={12} xs={12}>
+                            <div>
+                                <img className="card-img-top" src={museum} alt="museum" id="museum-pic" />
+                            </div>
+                            <div>
+                                <img className="card-img-top" src={coffee0} alt="coffee" id="coffee-pic-0" />
+                            </div>
+                            <div>
+                                <img className="card-img-top" src={pasta} alt="pasta" id="pasta-pic" />
+                            </div>
+                        </Col>
+                        <Col lg={3} md={6} sm={12} xs={12}>
+                            <div>
+                                <img className="card-img-top" src={people} alt="people" id="people-pic" />
+                            </div>
+                            <div>
+                                <img className="card-img-top" src={hike} alt="hike" id="hike-pic" />
+                            </div>
+                            <div>
+                                <img className="card-img-top" src={cafe} alt="cafe" id="cafe-pic" />
+                            </div>
+                        </Col>
+                        <Col id="search-filters" lg={6} md={12} sm={12} xs={12}>
+                            <img src={searchLogo} alt="Logo" class="searchLogo" id="searchLogo" />
+                            <form  onSubmit={this.handleSubmit}>
+                                <div  class="form-group">
+                                    <label for="inputdefault"></label>
+                                    <input id="keyword-bar" class="keywordBtn" id="inputdefault" type="text" placeholder="Type a keyword ... (hike, Chinese, etc.)" value={this.state.value} onChange={this.handleChange} />
                                 </div>
-                                <div>
-                                    <img className="card-img-top" src={coffee0} alt="coffee" id="coffee-pic-0" />
-                                </div>
-                                <div>
-                                    <img className="card-img-top" src={pasta} alt="pasta" id="pasta-pic" />
-                                </div>
-                            </Col>
-                            <Col lg={3} md={6} sm={12} xs={12}>
-                                <div>
-                                    <img className="card-img-top" src={people} alt="people" id="people-pic" />
-                                </div>
-                                <div>
-                                    <img className="card-img-top" src={hike} alt="hike" id="hike-pic" />
-                                </div>
-                                <div>
-                                    <img className="card-img-top" src={cafe} alt="cafe" id="cafe-pic" />
-                                </div>
-                            </Col>
-
-                            <Col id="search-filters" lg={6} md={12} sm={12} xs={12}>
-                                <img src={searchLogo} alt="Logo" class="searchLogo" id="searchLogo" />
-                                <form onSubmit={this.handleSubmit}>
-                                    <div class="form-group">
-                                        <label for="inputdefault"></label>
-                                        <input class="keywordBtn" id="inputdefault" type="text" placeholder="Type a keyword ... (hike, Chinese, etc.)" value={this.state.value} onChange={this.handleChange} />
-                                    </div>
-                                </form>
-                                <Row>
-                                    <label for="location" id="search-text" >Location</label>
-                                    <select class="options" id="location">
-                                        <option>Seattle, WA</option>
-                                        <option>Option 1</option>
-                                        <option>Option 2</option>
-                                    </select>
-                                </Row>
-                                <Row>
-                                    <p id="search-text">Price</p>
-                                    <Col lg={2} md={2} sm={2} xs={2}>
-                                        <button id ="dollarButton" type="button" class="dollarButton">
-                                            <span>&#36;</span>
-                                        </button>
-                                    </Col>
-                                    <Col lg={2} md={2} sm={2} xs={2}>
-                                        <button id ="dollarButton" type="button" class="dollarButton">
-                                            <span>&#36;&#36;</span>
-                                        </button>
-                                    </Col>
-                                    <Col lg={2} md={2} sm={2} xs={2}>
-                                        <button id ="dollarButton" type="button" class="dollarButton">
-                                            <span>&#36;&#36;&#36;</span>
-                                        </button>
-                                    </Col>
-                                    <Col lg={2} md={2} sm={2} xs={2}>
-                                        <button id ="dollarButton" type="button" class="dollarButton">
-                                            <span>&#36;&#36;&#36;&#36;</span>
-                                        </button>
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col lg={3} md={2} sm={3} xs={12}>  <p id="search-text">Distance</p> </Col>
-                                    <Col lg={2} md={2} sm={2} xs={3}>
-                                        <button class="distanceButton">5 mi</button>
-                                    </Col>
-                                    <Col lg={2} md={2} sm={2} xs={3}>
-                                        <button class="distanceButton">10 mi</button>
-                                    </Col>
-                                    <Col lg={2} md={2} sm={2} xs={3}>
-                                        <button class="distanceButton">15 mi</button>
-                                    </Col>
-                                    <Col lg={2} md={2} sm={2} xs={3}>
-                                        <button class="distanceButton">20 mi</button>
-                                    </Col>
-                                </Row>
-                                <div class="text-center">
+                            </form>
+                            <Row id="search-row">
+                                <label for="location" id="search-text" >Location</label>
+                                <select class="options" id="location">
+                                    <option>Seattle, WA</option>
+                                    <option>Option 1</option>
+                                    <option>Option 2</option>
+                                </select>
+                            </Row>
+                            <Row id="search-row">
+                                <p id="search-text">Price</p>
+                                <Col lg={2} md={2} sm={2} xs={2}>
+                                    <button id="dollarButton" type="button" class="dollarButton">
+                                        <span>&#36;</span>
+                                    </button>
+                                </Col>
+                                <Col lg={2} md={2} sm={2} xs={2}>
+                                    <button id="dollarButton" type="button" class="dollarButton">
+                                        <span>&#36;&#36;</span>
+                                    </button>
+                                </Col>
+                                <Col lg={2} md={2} sm={2} xs={2}>
+                                    <button id="dollarButton" type="button" class="dollarButton">
+                                        <span>&#36;&#36;&#36;</span>
+                                    </button>
+                                </Col>
+                                <Col lg={2} md={2} sm={2} xs={2}>
+                                    <button id="dollarButton" type="button" class="dollarButton">
+                                        <span>&#36;&#36;&#36;&#36;</span>
+                                    </button>
+                                </Col>
+                            </Row>
+                            <Row id="search-row">
+                                <p id="search-text">Distance</p>
+                                <Col lg={2} md={2} sm={2} xs={2}>
+                                    <button class="distanceButton">5 mi</button>
+                                </Col>
+                                <Col lg={2} md={2} sm={2} xs={2}>
+                                    <button class="distanceButton">10 mi</button>
+                                </Col>
+                                <Col lg={2} md={2} sm={2} xs={2}>
+                                    <button class="distanceButton">15 mi</button>
+                                </Col>
+                                <Col lg={2} md={2} sm={2} xs={2}>
+                                    <button class="distanceButton">20 mi</button>
+                                </Col>
+                            </Row>
+                            <div class="text-center">
                                 <button type="button" class="screwReviewButton" onClick={this.seeResult}>Screw that Review!</button>
-                                </div>
-                            </Col>
-                        </Row>
-                    ) : (
-                        <Row>
-                            <Result location="seattle" category={this.state.value}></Result>
-                        </Row>
-                    )}
-                </Container>
+                            </div>
+                        </Col>
+                    </Row>
+                ) : (
+                    <Row>
+                        <Result location="seattle" category={this.state.value}></Result>
+                    </Row>
+                )}
+            </Container>
         );
     }
 }
