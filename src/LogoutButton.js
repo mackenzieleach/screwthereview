3758e7e07fc084aa720d2a57f1239d1d2c630459
mmlea <leachm26@uw.edu>
2021-05-11 18:19:36 -0700

    Linting!

--- src/LogoutButton.js
+++ src/LogoutButton.js
@@ -1,14 +1,15 @@
-import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button id="auth0-logout" onClick={() => logout({ returnTo: window.location.origin })}>
+    <button type="button" id="auth0-logout" onClick={() => logout({ returnTo: window.location.origin })}>
       Log Out
     </button>
   );
 };
 
-export default LogoutButton; 
+export default LogoutButton;
