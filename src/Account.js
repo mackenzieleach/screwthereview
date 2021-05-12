6094b29d49f9acbc407ea63a50d03244cf79b3e8
mmlea <leachm26@uw.edu>
2021-05-11 20:25:47 -0700

    Linted Account

--- src/Account.js
+++ src/Account.js
@@ -1,20 +1,22 @@
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable react/jsx-filename-extension */
 import React, { Component } from 'react';
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class Account extends Component {
-    render() {
-        return (
-            <Container fluid className="page-container" id="page-container">
-                <Row>
-                    <Col>
-                    <h5>Account</h5>
-                    </Col>
-                </Row >
-            </Container >
-        );
-    }
+  render() {
+    return (
+      <Container fluid className="page-container" id="page-container">
+        <Row>
+          <Col>
+            <h5>Account</h5>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default Account;
