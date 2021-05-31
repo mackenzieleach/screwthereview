af4bf1b68bdcc9989c3c545dbf9044689d69f256
mmlea <leachm26@uw.edu>
2021-05-31 00:47:43 -0700

    Small Changes to Buttons

--- src/LoginButton.js
+++ src/LoginButton.js
@@ -5,7 +5,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button type="button" id="auth0-login" onClick={() => loginWithRedirect()}>Log In</button>;
+  return <button type="button" id="auth0-login" onClick={() => loginWithRedirect()}>Sign In</button>;
 };
 
 export default LoginButton;
