dfc3bec7ba47efcdcf976fd2e356283eeb1a4c7f
lulup2 <lulup2@uw.edu>
2021-05-31 15:36:00 -0700

    hours working and description updated

--- src/Result.js
+++ src/Result.js
@@ -4,7 +4,7 @@
 /* eslint-disable react/jsx-filename-extension */
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import Map from './Map.png';
+import MapImage from './Map.png';
 import server from './server';
 
 class Result extends Component {
@@ -33,6 +33,7 @@ class Result extends Component {
     })
       .then((response) => response.text())
       .then((json) => {
+        console.log(json);
         this.parseResult(json);
       });
   }
@@ -64,6 +65,7 @@ class Result extends Component {
       for (let i = 1; i < rCategories.length; i += 1) {
         sCategories += `, ${rCategories[i].title}`;
       }
+      const sHours = this.parseHours(result.hours);
       this.setState({
         name: result.name,
         imageURL: result.image_url,
@@ -72,8 +74,61 @@ class Result extends Component {
         address: result.location.display_address.join(' '),
         tags: sCategories,
         price: result.price,
-        hours: 'HOURS',
+        hours: sHours,
+        description: result.description,
       });
+    }
+
+    convertHours = (hour) => {
+      let result = '';
+      const iHour = parseInt(hour, 10);
+      let timeOfDay = ' am';
+
+      let hours = (iHour / 100);
+      // check time of day
+      if (hours === 0) {
+        hours = 12;
+      }
+      if (hours > 12) {
+        timeOfDay = ' pm';
+        hours -= 12;
+        // add zero for single digit
+        if (hours < 10) {
+          result = '0';
+        }
+      }
+      result += hours.toString();
+      result += ':';
+      const minutes = (iHour % 100);
+      // add zero for single digit
+      if (minutes < 10) {
+        result += '0';
+      }
+      result += minutes.toString();
+      result += timeOfDay;
+      return result;
+    }
+
+    parseHours = (hours) => {
+      let result = '';
+      const dayMap = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday',
+        'Friday', 'Saturday'];
+      let day;
+      let dayStart;
+      let dayEnd;
+      const hoursOpen = hours[0].open;
+      for (let i = 0; i < hoursOpen.length; i += 1) {
+        day = hoursOpen[i];
+        result += dayMap[day.day];
+        result += ': ';
+        dayStart = this.convertHours(day.start);
+        result += dayStart;
+        result += ' - ';
+        dayEnd = this.convertHours(day.end);
+        result += dayEnd;
+        result += '\n';
+      }
+      return result;
     }
 
     render() {
@@ -114,45 +169,14 @@ class Result extends Component {
             </Col>
             <Col sm={{ span: 6, offset: 1 }}>
               <div className="font-medium">About</div>
-              <div>More about this experience coming soon!</div>
+              <div>{this.state.description}</div>
               <Row>
                 <Col sm={7}>
-                  <img alt="map" src={Map} className="img-responsive fit-image col-12" />
+                  <img alt="map" src={MapImage} className="img-responsive fit-image col-12" />
                 </Col>
                 <Col sm={5}>
                   <div className="font-medium">Hours</div>
-                  <table id="result-hours-table" data-testid="result-hours-table">
-                    <tbody>
-                      <tr data-testid="ht-item">
-                        <td>Sunday</td>
-                        <td>10:30AM-9:30PM</td>
-                      </tr>
-                      <tr data-testid="ht-item">
-                        <td>Monday</td>
-                        <td>10:30AM-9:30PM</td>
-                      </tr>
-                      <tr data-testid="ht-item">
-                        <td>Tuesday</td>
-                        <td>10:30AM-9:30PM</td>
-                      </tr>
-                      <tr data-testid="ht-item">
-                        <td>Wednesday</td>
-                        <td>10:30AM-9:30PM</td>
-                      </tr>
-                      <tr data-testid="ht-item">
-                        <td>Thursday</td>
-                        <td>10:30AM-9:30PM</td>
-                      </tr>
-                      <tr data-testid="ht-item">
-                        <td>Friday</td>
-                        <td>10:30AM-9:30PM</td>
-                      </tr>
-                      <tr data-testid="ht-item">
-                        <td>Saturday</td>
-                        <td>10:30AM-9:30PM</td>
-                      </tr>
-                    </tbody>
-                  </table>
+                  <div className="hours">{this.state.hours}</div>
                 </Col>
               </Row>
               <Row>
